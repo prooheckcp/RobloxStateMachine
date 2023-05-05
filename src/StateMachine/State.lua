e852187076082e6c59a352a964d710654e3c3cbc
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2023-05-05 21:40:43 +0100

    Setting Virtual method

--- src/StateMachine/State.lua
+++ src/StateMachine/State.lua
@@ -1,8 +1,13 @@
+local Transition = require(script.Parent.Transition)
+
 local State = {}
 State.__index = State
 
 function State.new(): State
     local self = setmetatable({}, State)
+    
+    self.Name = "" :: string
+    self.Transitions = {} :: {Transition.Transition}
 
     return self
 end
--- src/StateMachine/Transition.lua
+++ src/StateMachine/Transition.lua
@@ -3,8 +3,17 @@ Transition.__index = Transition
 
 function Transition.new(): Transition
     local self = setmetatable({}, Transition)
+    self.TargetState = "" :: string
 
     return self
+end
+
+--[=[
+    Virtual method, should return true if it should change to this state
+    and false if it shouldn't
+]=]
+function Transition:OnDataChanged(): boolean
+    return false
 end
 
 export type Transition = typeof(Transition.new(...))
