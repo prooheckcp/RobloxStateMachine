2e684f9d0a560030db82d243dcf0bc68e358c873
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2023-12-02 20:20:28 +0000

    Creating wally.yaml

---
+++ ManageVersion.py
@@ -0,0 +1,32 @@
+import toml
+
+filename = "data.toml"
+version_key = "version"
+container = "package"
+
+def addVersion(version_string):
+    # Split the version string into parts
+    parts = version_string.split(".")
+
+    # Convert the last part to an integer and increment it
+    last_part = int(parts[-1])
+    new_last_part = last_part + 1
+
+    # Update the last part in the list of parts
+    parts[-1] = str(new_last_part)
+
+    # Join the parts back into a string
+    return ".".join(parts)
+
+# Load the TOML file
+with open(filename, "r") as f:
+    data = toml.load(f)
+
+# Increment the version
+if version_key in data[container]:
+    currentVersion = data[container][version_key]
+    data[container][version_key] = addVersion(currentVersion)
+
+# Save the updated TOML file
+with open(filename, "w") as f:
+    toml.dump(data, f)
